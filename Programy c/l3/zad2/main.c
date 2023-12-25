17cf8668cbd6c32041814b0d79b696967d90ecad
ShrikeBin <grahamka.ha@gmail.com>
2023-12-25 19:21:28 +0100

    small fix

---
+++ Programy c/l3/zad2/main.c
@@ -0,0 +1,14 @@
+#include "functions.h"
+
+int main()
+{
+    double a,b,eps;
+    printf("Enter range start: ");
+    scanf("%f", &a);
+    printf("Enter range end: ");
+    scanf("%f", &b);
+    printf("Enter precision: ");
+    scanf("%f", &eps);
+    printf("\nResult: ", result(a,b,eps));
+
+}
---
+++ Programy c/l3/zad3/phi.c
@@ -0,0 +1,19 @@
+#include <math.h>
+
+int phi(long int n)
+{
+int divn[n];
+int divtemp[n]; //dobra kur@ nie mam pomyslu dzisiaj
+int index = 0;
+
+for(int i; i<=(sqrt(n)+1);i++)
+{
+    if(n%i==0)
+    {
+        divn[index] = i;
+        divn[(index+1)] = n%i;
+        index = index + 2;
+    }
+}
+
+}
