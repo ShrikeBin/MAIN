04c9b6fee3d64aa4c9e6479938c581784bd6bd7e
ShrikeBin <grahamka.ha@gmail.com>
2024-01-02 16:51:58 +0100

    my best 2 friends - Smith & Wesson

--- Programy c/l3/zad3/phi.c
+++ Programy c/l3/zad3/phi.c
@@ -1,19 +1,51 @@
 #include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
 
-int phi(long int n)
+bool IsPrime(long int n)
 {
-int divn[n];
-int divtemp[n]; //dobra kur@ nie mam pomyslu dzisiaaj
-int index = 0;
-
-for(int i; i<=(sqrt(n)+1);i++)
-{
-    if(n%i==0)
+    if (n == 1)
     {
-        divn[index] = i;
-        divn[(index+1)] = n%i;
-        index = index + 2;
+        return false;
     }
+
+    int y = sqrt(n);
+    int sum = 0;
+
+    for (long int i = 2; i < y + 1; i++)
+    {
+        if ((n % i) == 0)
+        {
+            sum = sum + i + (n / i);
+        }
+    }
+
+    if (sum != 0)
+    {
+        return false;
+    }
+    else
+        return true;
 }
 
+long int phi(long int n)
+{
+    long int count = 0;
+
+    for (long int i = 1; i <= n; i++)
+    {
+        if (IsPrime(i))
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int main()
+{
+    long int n;
+    printf("Count up to: ");
+    scanf("%ld", &n);
+    printf("%ld\n", phi(n));
 }
