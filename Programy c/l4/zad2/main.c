fe1c7719f57ff3363947d9fc559fbf88213c0325
ShrikeBin <grahamka.ha@gmail.com>
2024-01-03 14:43:35 +0100

    pakuj sie do miski

---
+++ Programy c/l4/zad2/main.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdbool.h>
+
+bool check(int example, int red, int white, int try) // czy taki try moze być jesli na example odpowiedz była red i white
+{
+}
+
+int main()
+{
+    bool codes[6][6][6][6];
+    short example[4];
+    short red = 0;
+    short white = 0;
+    short try[4];
+
+    while (red != 4)
+    {
+        for (int a; a < 6; a++)
+        {
+            for (int b; b < 6; b++)
+            {
+                for (int c; c < 6; c++)
+                {
+                    for (int d; d < 6; d++)
+                    {
+                        if (!codes[a][b][c][d])
+                        {
+                            printf("%d%d%d%d?\n", a, b, c, d); // wypisz kod który może byc
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        printf("Red: ");
+        scanf("%d", red);
+        printf("White: ");
+        scanf("%d", white);
+
+        for (int a; a < 6; a++)
+        {
+            for (int b; b < 6; b++)
+            {
+                for (int c; c < 6; c++)
+                {
+                    for (int d; d < 6; d++)
+                    {
+                        if (codes[a][b][c][d])
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            codes[a][b][c][d] = check(example, red, white, try); // sprawdź które mogą po tamtych guessach
+                        }
+                    }
+                }
+            }
+        }
+    }
+    printf("I win!\n");
+}
