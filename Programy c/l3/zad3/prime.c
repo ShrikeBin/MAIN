7100429f9474ac429fafddbb7dbafbe7bc62d82e
ShrikeBin <grahamka.ha@gmail.com>
2024-01-03 16:04:16 +0100

    rakieta wie gdzie jest, ponieważ wie, gdzie jej ni...

--- Programy c/l3/zad3/prime.c
+++ Programy c/l3/zad3/prime.c
@@ -4,15 +4,14 @@
 bool IsCoPrime(long int n, long int k) // czy k względnie z n (assert n>k)
 {
 
-    if ((n == 1)||(n % k == 0))  // graniczny 
+    if ((n == 1) || (n % k == 0)) // graniczny
     {
         return false;
     }
 
-    int y = sqrt(k);
     int sum = 0;
 
-    for (long int i = 2; i < y + 1; i++)
+    for (long int i = 2; i < k + 1; i++)
     {
         if (((n % i) == 0) && ((k % i) == 0))
         {
